8a54799730b0676a1ebdfd748edfe50391b7d905
Martin Hartt <martinhartt1@gmail.com>
2017-08-10 23:25:08 +0100

    Integrate sample rule

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+import sampleRules from './rules/sample-rule';
+import { MessageResult, ResultStatus } from './ruleTypes';
+
 export default function app(message: string) {
-  return message;
+  const result: MessageResult = sampleRules.evaluate({
+    message: {
+      raw: message,
+    },
+  });
+
+  if (result.status == ResultStatus.Okay) {
+    return message;
+  } else {
+    throw new Error(result.warning);
+  }
 }

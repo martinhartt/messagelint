a54e09991f51e720f350f766583a23e2d6e9302d
Martin Hartt <martinhartt1@gmail.com>
2017-08-10 21:54:39 +0100

    Make simple command line interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+import * as program from "commander";
+
+program
+  .arguments('message')
+  .action(message => {
+    console.log('Results: ', message);
+  })
+  .parse(process.argv);

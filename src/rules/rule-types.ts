ac33172467d82b3508e4bd42b4287629174f1203
Martin Hartt <martinhartt1@gmail.com>
2017-08-11 23:03:02 +0100

    Define rule types

---
+++ src/rules/rule-types.ts
@@ -0,0 +1,43 @@
+export interface Message {
+  raw: string;
+}
+
+export interface Commit {
+  message: Message;
+}
+
+export enum ResultStatus {
+  Okay,
+  Modify,
+  ConfirmModify,
+  Reject,
+}
+
+export interface MessageResult {
+  status: ResultStatus;
+  warning?: string;
+  proposed?: string;
+}
+
+export interface RuleEvaluator {
+  (context: Commit): MessageResult;
+}
+
+export interface RuleMeta {
+  name: string;
+  key: string;
+  description: string;
+}
+
+export default interface Rule {
+  meta: RuleMeta;
+  evaluate: RuleEvaluator;
+};
+
+export interface Config {
+  [key: string]: boolean | object;
+}
+
+export interface RuleFactory {
+  (config: Config): Rule;
+}

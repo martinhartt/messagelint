72b81abcc1791f3c48c67161126e31c7ad4cf7d3
Martin Hartt <martinhartt1@gmail.com>
2017-08-16 20:39:45 +0100

    Fix tests

--- src/rules/load-rules.test.ts
+++ src/rules/load-rules.test.ts
@@ -6,7 +6,13 @@ import 'mocha';
 describe('loadRules', () => {
   it('returns all the rules with no config', () => {
     const result = loadRules();
+<<<<<<< HEAD
     expect(result.map(rule => rule.meta.key)).to.deep.equal(['no-whitespace-padding', 'first-letter-capital']);
+=======
+    expect(result.map(rule => rule.meta.key)).to.deep.equal([
+      'first-letter-capital',
+    ]);
+>>>>>>> Fix tests
   });
 
   it('does not return rules with a false value in config', () => {
@@ -22,6 +28,12 @@ describe('loadRules', () => {
         test: true,
       },
     });
+<<<<<<< HEAD
     expect(result.map(rule => rule.meta.key)).to.deep.equal(['no-whitespace-padding', 'first-letter-capital']);
+=======
+    expect(result.map(rule => rule.meta.key)).to.deep.equal([
+      'first-letter-capital',
+    ]);
+>>>>>>> Fix tests
   });
 });

39a8b48968881a3066b47eb7464ba2467d71a23f
Martin Hartt <martinhartt1@gmail.com>
2017-08-20 22:16:06 +0100

    Add command parameter

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,35 @@
+import { readFile, writeFile } from 'mz/fs';
+
 import loadRules from './rules/load-rules';
 import ruleRunner from './rules/rule-runner';
 import { ResultStatus } from './rules/rule-types';
 
-export default async function app(message: string) {
-  const rules = loadRules();
 
-  const result = await ruleRunner(rules, {
-    message: {
-      raw: message,
-    },
-  });
+export default async function app(command: string, message: string) {
+  switch (command) {
+    case 'setup':
+      console.log('Setting up better-commit...')
+      const hook = await readFile('./hooks/commit-msg');
+      console.log('hook is ', hook)
+      break;
+    case 'lint':
+      const rules = loadRules();
 
-  switch (result.status) {
-    case ResultStatus.Rejected:
-      throw new Error(result.warning);
-    case ResultStatus.Approved:
-      return message;
-    case ResultStatus.Modify:
-      return result.proposed;
+      const result = await ruleRunner(rules, {
+        message: {
+          raw: message,
+        },
+      });
+
+      switch (result.status) {
+        case ResultStatus.Rejected:
+        throw new Error(result.warning);
+        case ResultStatus.Approved:
+        return message;
+        case ResultStatus.Modify:
+        return result.proposed;
+      }
+      break;
   }
+
 }
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import * as program from 'commander';
 import app from './app';
 
 program
-  .arguments('message')
-  .action(message => {
-    app(message)
+  .arguments('command message')
+  .action((command, message) => {
+    app(command, message)
       .then(console.log)
       .catch(err =>
         console.error(`Your commit was rejected due to: \n\n${err.message}`),

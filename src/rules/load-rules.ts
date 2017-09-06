9ffddd5ea41e7920fda40760215aa504b66cc596
Martin Hartt <martinhartt1@gmail.com>
2017-09-06 22:38:10 +0100

    Integrate new rule types into loadRules

--- src/rules/load-rules.ts
+++ src/rules/load-rules.ts
@@ -1,16 +1,8 @@
-import { Config, Rule } from './rule-types';
+import { merge } from 'lodash';
+import { GlobalConfig, Rule } from './rule-types';
 
-const defaultConfig: Config = {
-  'no-whitespace-padding': true,
-  'first-word-verb': true,
-  'first-letter-capital': true,
-  'no-trailing-dot': true,
-};
-
-export default function loadRules(config: Config = {}): Rule[] {
-  const completeConfig = { ...defaultConfig, ...config };
-
-  return Object.keys(completeConfig)
-    .filter(ruleName => completeConfig[ruleName] !== false)
-    .map(ruleName => require('./' + ruleName).default(config[ruleName]));
+export default function loadRules(config: GlobalConfig): Rule[] {
+  return Object.keys(config.rules)
+    .filter(ruleName => config.rules[ruleName] !== false)
+    .map(ruleName => require('./' + ruleName).default(merge({}, config, config.rules[ruleName])));
 }

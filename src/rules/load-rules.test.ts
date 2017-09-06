9dae8806ec9ef62009ac9dd367807a70255ef037
Martin Hartt <martinhartt1@gmail.com>
2017-09-06 22:38:19 +0100

    Fix load rules tests

--- src/rules/load-rules.test.ts
+++ src/rules/load-rules.test.ts
@@ -1,12 +1,16 @@
 import loadRules from './load-rules';
+import { GlobalConfig } from './rule-types';
 
 import { expect } from 'chai';
+import { merge } from 'lodash';
 import 'mocha';
+
+const baseConfig = require('../../configs/base.json') as GlobalConfig; // tslint:disable-line
 
 describe('loadRules', () => {
   it('returns all the rules with no config', () => {
-    const result = loadRules();
-    expect(result.map(rule => rule.meta.key)).to.deep.equal([
+    const result = loadRules(baseConfig);
+    expect(result.map(rule => rule.meta.key)).to.have.members([
       'no-whitespace-padding',
       'first-word-verb',
       'first-letter-capital',
@@ -15,27 +19,22 @@ describe('loadRules', () => {
   });
 
   it('does not return rules with a false value in config', () => {
-    const result = loadRules({
-      'first-letter-capital': false,
-    });
-    expect(result.map(rule => rule.meta.key)).to.deep.equal([
-      'no-whitespace-padding',
-      'first-word-verb',
-      'no-trailing-dot',
-    ]);
+    const result = loadRules(
+      merge({}, baseConfig, {
+        rules: { 'first-letter-capital': false },
+      }),
+    );
+    expect(result.map(rule => rule.meta.key)).to.not.include('first-letter-capital');
   });
 
   it('does return rules with a specific config', () => {
-    const result = loadRules({
-      'first-letter-capital': {
-        test: true,
-      },
-    });
-    expect(result.map(rule => rule.meta.key)).to.deep.equal([
-      'no-whitespace-padding',
-      'first-word-verb',
-      'first-letter-capital',
-      'no-trailing-dot',
-    ]);
+    const result = loadRules(
+      merge({}, baseConfig, {
+        'first-letter-capital': {
+          test: true,
+        },
+      }),
+    );
+    expect(result.map(rule => rule.meta.key)).to.include('first-letter-capital');
   });
 });
